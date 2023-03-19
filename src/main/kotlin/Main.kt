253d5eae770f97b3fc39d479c3b50b21328e127a
Richard Tichý <tichy@artify.zone>
2023-03-19 16:07:06 +0100

    Implement solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
+// solution to the problem Plus or Minus at Codeforces in Kotlin
 fun main() {
-    println("Hello, World!")
+    (1..readln().toInt()).forEach { _ ->
+        val (a, b, c) = readln().split(" ").map { it.toInt() }
+        println(if(a + b == c) "+" else "-")
+    }
 }
